34bf1e06017eb93cf4882bcf879c42c6d88acddf
Soe Sandar Win <soesandarwin2201@gmail.com>
2023-04-15 22:38:37 +0630

    add the protected branch to make sure user can't see the page before they login

--- src/App.js
+++ src/App.js
@@ -9,27 +9,25 @@ import ReserveForm from "./pages/Reservation/reserveForm";
 import SignUp from "./pages/Signup/signup";
 import Login from "./pages/login/login";
 import Splash from "./pages/splash/splash";
+import ProtectedRoute from "./components/route/protectedroute";
 
 function App() {
   return (
-    <>
-      <header>
-        <Router>
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/signout" element={<SignOut />} />
-            <Route path="/addHouse" element={<AddHouse />} />
-            <Route path="/deleteHouse" element={<DeleteHouse />} />
-            <Route path="/houseDetails" element={<HouseDetails />} />
-            <Route path="/reserveform" element={<ReserveForm />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<SignUp />} />
-            <Route path="/splash" element={<Splash />} />
-          </Routes>
-        </Router>
-      </header>
-    </>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/signout" element={<SignOut />} />
+        <Route path="/" element={<ProtectedRoute />}>
+          <Route exact path="/home" element={<Home />} />
+          <Route path="/addHouse" element={<AddHouse />} />
+          <Route path="/deleteHouse" element={<DeleteHouse />} />
+          <Route path="/houseDetails" element={<HouseDetails />} />
+          <Route path="/reserveform" element={<ReserveForm />} />
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
