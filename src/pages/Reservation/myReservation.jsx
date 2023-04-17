266fdd5da6da1f33998b8ccb7dc6fec149357e17
Nabeel Akbar <studiosnabeel@gmail.com>
2023-04-18 03:11:52 +0500

    Usestate

--- src/pages/Reservation/myReservation.jsx
+++ src/pages/Reservation/myReservation.jsx
@@ -2,3 +2,9 @@ import React, { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { addReservation } from '../../redux/reservation/reservationSlice';
 
+const ReserveHouse = () => {
+  const [house, setHouse] = useState({
+    start_date: '',
+    end_date: '',
+  });
+};
