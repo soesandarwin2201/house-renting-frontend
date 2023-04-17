8cad5ac7bbeb4f474b292af6ea9ef5c3af13a126
anyepeter <petzyrockchendi@gmail.com>
2023-04-17 19:30:44 +0100

    create form to reserve a house

---
+++ src/pages/House/homeReserved.jsx
@@ -0,0 +1,58 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { useNavigate, useParams } from "react-router";
+import { addReservedHouse } from "../../redux/reservation/reservationSlice";
+
+const HomeReserve = () => {
+    const { id } = useParams()
+  const [reservation, setReservation] = useState({
+    start_date: "",
+    end_date: "",
+    house_id: id
+  });
+
+  const inputValue = (e) => {
+    setReservation({
+      ...reservation,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault(); 
+    console.log(reservation)
+     dispatch(addReservedHouse( reservation ))
+     navigate("/reservations");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Start Date:
+        <input
+          type="date"
+          name="start_date"
+          onChange={inputValue}
+          required
+        />
+      </label>
+      <label>
+        End Date:
+        <input
+          type="date"
+          name="end_date"
+          onChange={inputValue}
+          required
+        />
+      </label>
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+export default HomeReserve;
