26adec6da43e5592816c3388166bbde44085843a
Nabeel Akbar <studiosnabeel@gmail.com>
2023-04-18 03:12:45 +0500

    Add useEffect hook

--- src/pages/Reservation/myReservation.jsx
+++ src/pages/Reservation/myReservation.jsx
@@ -8,3 +8,22 @@ const ReserveHouse = () => {
     end_date: '',
   });
 };
+const dispatch = useDispatch();
+
+useEffect(() => {
+  // Fetch API data and update the state of houses
+  const token = localStorage.getItem('token'); // Replace with your actual authentication token
+  fetch('http://localhost:3000/houses', {
+    headers: {
+      Authorization: `Bearer ${token}`,
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then((data) => setHouse(data));
+}, []);
