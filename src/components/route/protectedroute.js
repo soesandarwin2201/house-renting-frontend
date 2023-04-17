680c7c2a97f8871607bea5bfb965c7fd054d67f7
Nabeel Akbar <studiosnabeel@gmail.com>
2023-04-17 14:31:20 +0500

    Modify protected route file

--- src/components/route/protectedroute.js
+++ src/components/route/protectedroute.js
@@ -1,15 +1,21 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+
 import { useSelector } from "react-redux";
 import Splash from "../../pages/splash/splash";
 
 const ProtectedRoute = () => {
-  const token = useSelector((state) => state.login.token);
-  return (
-    !token || token.length <= 0 ? (
-      <Splash />
-    ) : <Outlet />
-  );
+  const navigate = useNavigate();
+  const loginToken = useSelector((state) => state.login.token);
+  const signupToken = useSelector((state) => state.signup.token);
+  const token = loginToken || signupToken;
+
+  if (!token || token.length <= 0) {
+   
+    return <Splash />;
+  }
+
+  return <Outlet />;
 };
 
 export default ProtectedRoute;
