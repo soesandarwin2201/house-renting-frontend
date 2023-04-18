d57c6a432533e4c05ed0a7a6c2352420b3a1f0f0
Nabeel Akbar <studiosnabeel@gmail.com>
2023-04-18 20:44:10 +0500

    Call useEffect hook

--- src/pages/Reservation/reserveForm.jsx
+++ src/pages/Reservation/reserveForm.jsx
@@ -1,17 +1,21 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
-import React, { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { fetchHouses } from "../../redux/house/houseSlice";
-import { addReservation } from "../../redux/reservation/reservationSlice";
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchHouses } from '../../redux/house/houseSlice';
+import { addReservation } from '../../redux/reservation/reservationSlice';
 
 const ReserveForm = () => {
-  const houses = useSelector((state) => state.houses.houses)
+  const houses = useSelector((state) => state.houses.houses);
   const [reservation, setReservation] = useState({
-    state_date: "",
-    end_date: "",
-    house_id: ""
-  })
-  const dispatch = useDispatch()
-}
+    state_date: '',
+    end_date: '',
+    house_id: '',
+  });
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const data = dispatch(fetchHouses());
+  }, [dispatch]);
+};
 
 export default ReserveForm;
