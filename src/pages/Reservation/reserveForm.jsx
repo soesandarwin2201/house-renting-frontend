6cf9b12e2c0173c9a786cbbe04aec49ba7661036
Nabeel Akbar <studiosnabeel@gmail.com>
2023-04-18 20:40:39 +0500

    Add component

--- src/pages/Reservation/reserveForm.jsx
+++ src/pages/Reservation/reserveForm.jsx
@@ -3,3 +3,9 @@ import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchHouses } from "../../redux/house/houseSlice";
 import { addReservation } from "../../redux/reservation/reservationSlice";
+
+const ReserveForm = () => {
+
+}
+
+export default ReserveForm;
