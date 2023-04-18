66bcee1e73810997dd4d0acd174ccd35a2526e93
Nabeel Akbar <studiosnabeel@gmail.com>
2023-04-18 20:43:05 +0500

    call useState and useDispatch func

--- src/pages/Reservation/reserveForm.jsx
+++ src/pages/Reservation/reserveForm.jsx
@@ -5,7 +5,13 @@ import { fetchHouses } from "../../redux/house/houseSlice";
 import { addReservation } from "../../redux/reservation/reservationSlice";
 
 const ReserveForm = () => {
-
+  const houses = useSelector((state) => state.houses.houses)
+  const [reservation, setReservation] = useState({
+    state_date: "",
+    end_date: "",
+    house_id: ""
+  })
+  const dispatch = useDispatch()
 }
 
 export default ReserveForm;
