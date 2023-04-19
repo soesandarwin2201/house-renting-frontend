0c948fd63a559702056888d75b0ef1daee3671c4
Nabeel Akbar <studiosnabeel@gmail.com>
2023-04-19 23:09:25 +0500

    Remove consol log

--- src/pages/Reservation/myReservation.jsx
+++ src/pages/Reservation/myReservation.jsx
@@ -1,33 +1,40 @@
-
-import { useEffect } from "react"
-import { useDispatch, useSelector } from "react-redux"
-import { fatchReservation } from "../../redux/reservation/reservationSlice"
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fatchReservation } from '../../redux/reservation/reservationSlice';
 
 const MyReservation = () => {
-const reservation = useSelector((state) => state.reservation.reservations)
-const dispatch = useDispatch()
+  const reservation = useSelector((state) => state.reservation.reservations);
+  const dispatch = useDispatch();
 
-useEffect( () => {
-  dispatch(fatchReservation())
-},[dispatch])
-console.log(reservation)
+  useEffect(() => {
+    dispatch(fatchReservation());
+  }, [dispatch]);
   return (
     <div>
       <h1>My Reservations</h1>
       <ul>
-      {reservation.map((house) => (
-            <li key={house.id} className="slide">
-              <h1>House name{house.name}</h1>
-             <p>start date {house.reservation.start_date}</p>
-              <p>End date {house.reservation.end_date}</p>
-              <br/>
-            </li>
-          ))}
+        {reservation.map((house) => (
+          <li key={house.id} className="slide">
+            <h1>
+              House name
+              {house.name}
+            </h1>
+            <p>
+              start date
+              {' '}
+              {house.reservation.start_date}
+            </p>
+            <p>
+              End date
+              {' '}
+              {house.reservation.end_date}
+            </p>
+            <br />
+          </li>
+        ))}
       </ul>
     </div>
-  )
-}
+  );
+};
 
-export default MyReservation
-
-   
+export default MyReservation;
