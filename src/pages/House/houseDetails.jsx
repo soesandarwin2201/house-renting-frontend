9ee58fe878b98d8e355a62ba424b0b911473f4fc
Soe Sandar Win <soesandarwin2201@gmail.com>
2023-04-19 11:44:26 +0630

    Create the mobile version for the deatil page

--- src/pages/House/houseDetails.jsx
+++ src/pages/House/houseDetails.jsx
@@ -2,10 +2,10 @@ import React, { useEffect } from "react";
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { useNavigate, useParams } from "react-router";
 import { useDispatch, useSelector } from "react-redux";
-import { showHouse } from "../../redux/house/houseSlice";
 import { IconContext } from 'react-icons';
 import * as FcIcon from 'react-icons/fc';
 import * as AiIcon from 'react-icons/ai';
+import { showHouse } from "../../redux/house/houseSlice";
 import "./house.css";
 
 function HouseDetails() {
@@ -19,36 +19,50 @@ function HouseDetails() {
   }, [dispatch, id]);
   return (
     <IconContext.Provider value={{ color: '#fff' }}>
-    <section id="detail-container">
-      <div key={id} id="card-container">
-      <div id="img-container">
-      <img
-            src={house?.image}
-            alt={house?.name}
-            id="detail-img"
-          />
-      </div>
-      <div id="detail-info">
-        <div className="house-address">
-      <h1 id="house-name">{house?.name}</h1>
-      <p id="location">{house?.location}</p>
+      <section id="detail-container">
+        <div key={id} id="card-container">
+          <div id="img-container">
+            <img
+              src={house?.image}
+              alt={house?.name}
+              id="detail-img"
+            />
+          </div>
+          <div id="detail-info">
+            <div className="house-address">
+              <h1 id="house-name">{house?.name}</h1>
+              <p id="location">{house?.location}</p>
+            </div>
+            <div className="price-container">
+              <li className="border">
+                <p className="info-name">Price: </p>
+                <p id="price">
+                  $
+                  {house?.price}
+                </p>
+              </li>
+              <li className="border">
+                <p className="info-name">Bedrooms: </p>
+                <p id="bed-room">
+                  {house?.bedroom_number}
+                  {' '}
+                  bds
+                </p>
+              </li>
+            </div>
+            <p id="description">
+              <span className="detail">Detail:</span>
+              {' '}
+              {house?.description}
+            </p>
+            <button id="reserver-btn" type="button" onClick={() => { navigate(`/houses/${house?.id}/reservations`); }}>
+              Reserve
+              <FcIcon.FcOvertime id="reserve-icon" />
+            </button>
+          </div>
         </div>
-        <div className="price-container">
-          <li className="border">
-            <p className="info-name">Price: </p>
-          <p id="price">${house?.price}</p>
-          </li>
-          <li className="border">
-            <p className="info-name">Bedrooms: </p>
-          <p id="bed-room">{house?.bedroom_number} bds</p>
-          </li>
-        </div>
-        <p id="description"><span className="detail">Detail:</span> {house?.description}</p>
-        <button id="reserver-btn" type="button" onClick={() => { navigate(`/houses/${house?.id}/reservations`); }}>Reserve <FcIcon.FcOvertime id="reserve-icon" /></button>
-      </div>
-      </div>
-      <button type="type" className="back" onClick={() => navigate(`/home`)}><AiIcon.AiFillCaretLeft /></button>
-    </section>
+        <button type="type" className="back" onClick={() => navigate(`/home`)}><AiIcon.AiFillCaretLeft /></button>
+      </section>
     </IconContext.Provider>
   );
 }
