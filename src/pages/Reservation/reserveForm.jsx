40377aa6495f6b06a45461f1f4f7e631fcb5e628
anyepeter <petzyrockchendi@gmail.com>
2023-04-19 12:21:38 +0100

    add reservation form

--- src/pages/Reservation/reserveForm.jsx
+++ src/pages/Reservation/reserveForm.jsx
@@ -1,36 +1,48 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchHouses } from "../../redux/house/houseSlice";
+import { addReservation } from "../../redux/reservation/reservationSlice";
 
 const ReserveForm = () => {
-  const [startDate, setStartDate] = useState("");
-  const [endDate, setEndDate] = useState("");
+  const houses =  useSelector((state) => state.houses.houses)
+  const [reservation, setReservation] = useState({
+    start_date: "",
+    end_date: "",
+    house_id: ""
+  })
 
-  const handleStartDateChange = (e) => {
-    const { value } = e.target;
-    setStartDate(value);
-  };
+  const dispatch = useDispatch()
 
-  const handleEndDateChange = (e) => {
-    const { value } = e.target;
-    setEndDate(value);
+  useEffect(() => {
+      const data =  dispatch(fetchHouses());
+      console.log(data)
+  }, [dispatch]);
+ 
+  const inputValue = (e) => {
+    setReservation({
+      ...reservation,
+      [e.target.name]: e.target.value,
+    });
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // You can perform form submission logic here, using the startDate and endDate state values
-    // console.log('Submitted start date:', startDate);
-    // console.log('Submitted end date:', endDate);
+    console.log(reservation)
+    dispatch(addReservation(reservation));
+     navigate("/reservations");
   };
+
 
   return (
     <form onSubmit={handleSubmit}>
-      <label className="reservation-form">
+      <label>
         Start Date:
         <input
           type="date"
-          className="reserve-input"
-          value={startDate}
-          onChange={handleStartDateChange}
+          name="start_date"
+          onChange={inputValue}
+         
           required
         />
       </label>
@@ -38,12 +50,27 @@ const ReserveForm = () => {
         End Date:
         <input
           type="date"
-          className="reserve-input"
-          value={endDate}
-          onChange={handleEndDateChange}
+          name="end_date"
+          onChange={inputValue}
+         
           required
         />
       </label>
+
+      <label for="cars">Choose a house:</label>
+<select id="cars" name="house_id" onChange={inputValue} form="carform">
+  {
+     houses.map((house) => {
+      return(
+        <option key={house.id} value={house.id}>
+          {house.name}
+        </option>
+      )
+     })
+  }
+ 
+</select>
+
       <button type="submit">Submit</button>
     </form>
   );
