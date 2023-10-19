caf2c802c8402b56cd12e34a4d745f4f78667e08
Soe Sandar Win <soesandarwin2201@gmail.com>
2023-10-19 12:11:30 +0630

    test the app

--- src/redux/apiEndpoints.js
+++ src/redux/apiEndpoints.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:3000/";
+export const BASE_URL = "http://0.0.0.0:10000/";
 export const LOGIN = `${BASE_URL}/login`;
 export const SIGNIN = `${BASE_URL}/users`;
 export const GETHOUSES = `${BASE_URL}/houses`;
